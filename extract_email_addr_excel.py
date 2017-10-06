804bbfb91b4018d8ffa5e22d859d8adf6956b8b9
Colin Turner <koolspin245@gmail.com>
2017-10-05 23:27:20 -0400

    Cleanup and comments

--- extract_email_addr_excel.py
+++ extract_email_addr_excel.py
@@ -4,6 +4,12 @@ import argparse
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
+
+# This utility will iterate through each row and column in an Excel spreadsheet to extract email addresses.
+# The extracted addresses will be written to a new spreadsheet
+# Author: Colin Turner (koolspin)
+# Dependencies: openpyxl
+
 def validate_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Path to the input xlsx file - containing email addresses")
@@ -15,10 +21,11 @@ def validate_args():
     else:
         return True, args.input, args.output
 
+
 def extract_email_addresses(in_file, out_file):
-    newWb = Workbook()
-    newWs = newWb.active
-    newRow = 1
+    new_wb = Workbook()
+    new_ws = new_wb.active
+    new_row = 1
 
     wb = load_workbook(in_file)
     sheet_collection = wb.sheetnames
@@ -26,7 +33,7 @@ def extract_email_addresses(in_file, out_file):
         ws = wb[sheet_collection[0]]
         for row in ws.rows:
             for col in row:
-                #print('Value of col: {0}'.format(col))
+                # print('Value of col: {0}'.format(col))
                 if col is not None and col.value is not None and isinstance(col.value, str):
                     # match = re.search(r'[\w\.-]+@[\w\.-]+', d.value)
                     match = re.search(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b', col.value)
@@ -34,10 +41,10 @@ def extract_email_addresses(in_file, out_file):
                         g = match.group(0)
                         if g is not None:
                             print('Matched email: [{0}]'.format(g))
-                            newWs.cell(row=newRow, column=1).value = g
-                            newRow = newRow + 1
+                            new_ws.cell(row=new_row, column=1).value = g
+                            new_row = new_row + 1
 
-        newWb.save(out_file)
+        new_wb.save(out_file)
 
 res = validate_args()
 if res[0]:
